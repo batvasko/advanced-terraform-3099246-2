f49216dd3499e1eeef92171d70fb9d1c33a37b23
batvasko <vvasilevpz@gmail.com>
2024-03-18 18:49:57 +0200

    terraform cloud my vars

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   cloud {
-    organization = "AdvancedTerraform" # CHANGE TO YOUR ORG NAME
+    organization = "batvasko03" # CHANGE TO YOUR ORG NAME
     hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
 
     workspaces {
-      name = "advanced-terraform-3099246"
+      name = "advanced-terraform-3099246-2"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 ### VARIABLES
 variable "project-id" {
   type = string
+  default = "storied-scarab-408814"
 }
 
 variable "org_id" {
@@ -11,12 +12,12 @@ variable "org_id" {
 
 variable "region" {
   type = string
-  default = "us-central1"
+  default = "europe-central2"
 }
 
 variable "zone" {
   type = string
-  default = "us-central1-a"
+  default = "europe-central2-a"
 }
 
 variable "subnet-name" {
@@ -26,7 +27,7 @@ variable "subnet-name" {
 
 variable "subnet-cidr" {
   type = string
-  default = "10.127.0.0/20"
+  default = "10.1.0.0/20"
 }
 
 variable "private_google_access" {
