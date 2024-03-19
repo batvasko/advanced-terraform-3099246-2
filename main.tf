27ac30090702f93bc65662d40638dcfed3069a2d
batvasko <vvasilevpz@gmail.com>
2024-03-19 14:24:43 +0200

    add mig

--- main.tf
+++ main.tf
@@ -5,69 +5,69 @@ provider "google" {
   zone    = var.zone
 }
 
-### COMPUTE
-## NGINX PROXY
-resource "google_compute_instance" "nginx_instance" {
-  name         = "nginx-proxy"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  tags = var.compute-source-tags
+# ### COMPUTE
+# ## NGINX PROXY
+# resource "google_compute_instance" "nginx_instance" {
+#   name         = "nginx-proxy"
+#   machine_type = var.environment_machine_type[var.target_environment]
+#   labels = {
+#     environment = var.environment_map[var.target_environment]
+#   }
+#   tags = var.compute-source-tags
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-    access_config {
+#   network_interface {
+#     network = data.google_compute_network.default.self_link
+#     subnetwork = google_compute_subnetwork.subnet-1.self_link
+#     access_config {
   
-    }
-  }
-}
+#     }
+#   }
+# }
 
-module "webservers" {
-  source = "./modules/webservers"
-  project_id = var.project-id
-  server_settings = var.environment_instance_settings
-  region = var.region
-  zone = var.zone
-  network_interface = {
-      network = data.google_compute_network.default.self_link,
-      subnetwork = google_compute_subnetwork.subnet-1.self_link
-      }
-}
+# module "webservers" {
+#   source = "./modules/webservers"
+#   project_id = var.project-id
+#   server_settings = var.environment_instance_settings
+#   region = var.region
+#   zone = var.zone
+#   network_interface = {
+#       network = data.google_compute_network.default.self_link,
+#       subnetwork = google_compute_subnetwork.subnet-1.self_link
+#       }
+# }
 
-## DB
-resource "google_compute_instance" "mysqldb" {
-  name         = "mysqldb"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
+# ## DB
+# resource "google_compute_instance" "mysqldb" {
+#   name         = "mysqldb"
+#   machine_type = var.environment_machine_type[var.target_environment]
+#   labels = {
+#     environment = var.environment_map[var.target_environment]
+#   }
   
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }  
-}
+#   network_interface {
+#     network = data.google_compute_network.default.self_link
+#     subnetwork = google_compute_subnetwork.subnet-1.self_link
+#   }  
+# }
 
-## REDIS
-resource "google_redis_instance" "redis" {
-  name = var.environment_instance_settings[var.target_environment].redis.name
-  tier = var.environment_instance_settings[var.target_environment].redis.tier
-  memory_size_gb = var.environment_instance_settings[var.target_environment].redis.memory_size_gb
-  location_id = var.zone
-  authorized_network = data.google_compute_network.default.id
-}
+# ## REDIS
+# resource "google_redis_instance" "redis" {
+#   name = var.environment_instance_settings[var.target_environment].redis.name
+#   tier = var.environment_instance_settings[var.target_environment].redis.tier
+#   memory_size_gb = var.environment_instance_settings[var.target_environment].redis.memory_size_gb
+#   location_id = var.zone
+#   authorized_network = data.google_compute_network.default.id
+# }
 
